33d7c913cfe6da55a8d8feb75240109bdff112f2
Henrry <jenrruhndz@gmail.com>
2020-10-15 12:32:40 -0600

    testung init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from "react";
+import "./index.css"
 
-function App() {
+import { BrowserRouter as Router, Route, Link, Switch, Redirect, useHistory, useParams, useLocation } from "react-router-dom";
+
+export default function App() {
+  const name = 'John Doe'
+  const isAuthenticated = true
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Router>
+    <main>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to={`/about/${name}`}>About</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+        </ul>
+      </nav>
+
+    <Switch>
+      <Route path="/" exact component={Home} />
+      {
+      isAuthenticated ? 
+      <>
+      <Route path="/about/:name"  component={About} />
+      <Route path="/contact"  component={Contact} />
+      </> : <Redirect to="/" />
+      }
+    </Switch>
+
+    </main>
+</Router>
   );
 }
 
-export default App;
+const Home = () => (
+  <Fragment>
+    <h1>Home</h1>
+    Hommie
+  </Fragment>
+  );
+
+
+  const About = () => {
+    const { name } = useParams()
+    return (
+    // props.match.params.name
+    <Fragment>
+      { name !== 'John Doe' ? <Redirect to="/" /> : null }
+      <h1>About {name}</h1>
+      <Route component={Contact} />
+    </Fragment>
+  )
+  };
+/*const About = ({match:{params:{name}}}) => (
+  <Fragment>
+    { name !== 'John Doe' ? <Redirect to="/" /> : null }
+    <h1>About {name}</h1>
+    About it
+  </Fragment>
+  );*/
+
+/*const Contact = ({history}) => (
+  <Fragment>
+    <h1>Contact</h1>
+    <button onClick={() => history.push('/') } >Go to home</button>
+    Contact it
+  </Fragment>
+  );*/
+
+  const Contact = () => {
+    const { pathname } = useLocation();
+    
+    return (
+      <Fragment>
+        <h1>Contact</h1>
+        <p>Current URL: {pathname}</p>
+      </Fragment>
+      )
+      };
